253ec1724197b700730c877eeb756cd9ce2c5041
ChamathkaM <67134546+ChamathkaM@users.noreply.github.com>
2025-10-30 23:06:53 +0200

    improved solution

--- Question_27.py
+++ Question_27.py
@@ -1,30 +1,8 @@
-class Solution(object):
-    def removeElement(self, nums, val):
-        """
-        :type nums: List[int]
-        :type val: int
-        :rtype: int
-        """
-        l=0
-        r=len(nums)-1
+        k = 0
 
-        if len(nums) == 0:
-            return 0
-
-        while l < r:
-            if nums[l] == val:
-                if nums[r] != val:
-                    nums[l] = nums[r]
-                    nums[r] = val 
-                    l += 1
-                r -= 1
-            else:
-                l += 1
-        if nums[l] != val:     
-            return l+1
-        else: 
-            if l>=r:
-                return l
-            return 0
-
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
         
+        return k
