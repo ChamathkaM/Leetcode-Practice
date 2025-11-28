30960c1968bd0635c279387b153e6aea35512e53
ChamathkaM <67134546+ChamathkaM@users.noreply.github.com>
2025-11-28 18:32:38 +0200

    Solution for Happy number

---
+++ Question_202.py
@@ -0,0 +1,14 @@
+        # tol=0
+
+        # while n != 1:
+        #     if n in seen:
+        #         return False
+        #     seen.add(n)
+        #     sumval=0
+        #     for i in str(n):   
+        #         sumval += int(i)**2
+        #         print(n)
+        #     n = sumval
+        #     print(n)
+            
+        # return True
