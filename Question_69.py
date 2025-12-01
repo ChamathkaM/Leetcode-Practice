64826b3bb794b7fece026f50e90395f1946ea5a5
ChamathkaM <67134546+ChamathkaM@users.noreply.github.com>
2025-12-01 22:48:31 +0200

    Unsuccessful solution to Sqrt(x)

---
+++ Question_69.py
@@ -0,0 +1,19 @@
+#not a successful solution
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        num = 1
+        if x == 0:
+            return 0
+        if x == 1:
+            return 1
+        for i in range(2,x+1):
+            if i*i == x:
+                return i
+            elif i*i < x:
+                num = i 
+            else:
+                return num
