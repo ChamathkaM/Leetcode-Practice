8444d9a8c859ec2e9c3903f3a2b0e8713c411e4e
ChamathkaM <67134546+ChamathkaM@users.noreply.github.com>
2025-12-01 22:58:46 +0200

    a working solution to sqrt(x)

--- Question_69.py
+++ Question_69.py
@@ -1,20 +1,22 @@
-#not a successful solution
-# proper solution should be implemented with by considering mid point
 class Solution(object):
     def mySqrt(self, x):
         """
         :type x: int
         :rtype: int
         """
-        num = 1
+    
         if x == 0:
             return 0
-        if x == 1:
-            return 1
-        for i in range(2,x+1):
-            if i*i == x:
-                return i
-            elif i*i < x:
-                num = i 
+        first, last = 1, x
+
+        while first <= last:
+            mid =  first +(last-first)/2
+
+            if x / mid == mid:
+                return mid
+            elif x / mid < mid:
+                last = mid -1 
             else:
-                return num
+                first = mid + 1
+        return last
+        
