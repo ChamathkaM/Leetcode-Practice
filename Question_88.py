45466a01d65ff031aa1b55247767d687ee850386
ChamathkaM <67134546+ChamathkaM@users.noreply.github.com>
2025-12-05 22:20:07 +0200

    Successful attempt to merge sorted array

--- Question_88.py
+++ Question_88.py
@@ -11,17 +11,15 @@ class Solution(object):
         num2ind = n-1
         num1len=(m+n)-1
 
-        while num1len>=0 and num2ind>=0 and num1ind>=0:
+        while num2ind >= 0:
 
-            if nums2[num2ind] > nums1[num1ind]:
+            if num1ind >= 0 and nums1[num1ind] > nums2[num2ind]:
+                nums1[num1len] = nums1[num1ind]
+                num1ind -= 1
+            else:
                 nums1[num1len] = nums2[num2ind]
                 num2ind -= 1
-                num1len -= 1
-
-            else:
-                print(num1len,num1ind)
-                nums1[num1len] = nums1[num1ind]
-                num1len -= 1
+            num1len -= 1
         return nums1
 
 
