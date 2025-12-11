9942e4172d3d58cce8293879d9142eb4f9782d92
ChamathkaM <67134546+ChamathkaM@users.noreply.github.com>
2025-12-11 21:52:47 +0200

    Update Question_205.py

--- Question_205.py
+++ Question_205.py
@@ -1,1 +1,14 @@
+#wrong solution
+if len(s) != len(t):
+            return False
 
+        
+        
+        # for i in range(len(s)-1):
+        #     if s[i] == s[i+1]:
+        #         if t[i] != t[i+1]:
+        #             return False
+        #     else:
+        #         if t[i] == t[i+1]:
+        #             return False
+        # return True
